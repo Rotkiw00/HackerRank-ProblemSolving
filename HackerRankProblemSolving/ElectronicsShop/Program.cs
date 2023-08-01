4161ee76af22f77d4d5ba24022d80e453b2bf55e
Rotkiw00 <wkalaga73@gmail.com>
2023-08-01 22:39:53 +0200

    Add two new solved exercises

---
+++ HackerRankProblemSolving/ElectronicsShop/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ElectronicsShop;
+class Program
+{
+    /*
+     * Complete the getMoneySpent function below.
+     */
+    static int getMoneySpent(int[] keyboards, int[] drives, int b)
+    {
+        List<int> costs = new();
+        foreach (var keyboard in keyboards)
+        {
+            foreach (var drive in drives)
+            {
+                costs.Add(keyboard + drive);
+            }
+        }
+        return costs.Where(c => c <= b).DefaultIfEmpty(-1).Max();
+    }
+
+    static void Main(string[] args)
+    {
+        int i = getMoneySpent(new int[] { 4 }, new int[] { 5 }, 5);
+        Console.WriteLine(i);
+    }
+}
+
---
+++ HackerRankProblemSolving/HurdleRace/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace HurdleRace;
+class Program
+{
+    /*
+     * Complete the 'hurdleRace' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER k
+     *  2. INTEGER_ARRAY height
+     */
+
+    public static int hurdleRace(int k, List<int> height)
+    {
+        if (k < height.Max())
+        {
+            return height.Max() - k;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        //test1
+        int t1 = hurdleRace(4, new List<int>() { 1, 6, 3, 5, 2 });
+
+        //test2
+        int t2 = hurdleRace(7, new List<int>() { 2, 5, 4, 5, 2 });
+    }
+}
+
